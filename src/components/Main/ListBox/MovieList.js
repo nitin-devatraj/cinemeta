9b6d98a891b7ebc2d8feaa21532223551f308ff2
nitin devatraj <nitindevatraj@protonmail.com>
2023-12-31 20:01:16 +0530

    implement initial static app

---
+++ src/components/Main/ListBox/MovieList.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Movie from "./Movie";
+
+export default function MovieList({ tempMovieData }) {
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import ListBox from "./ListBox/ListBox";
+import WatchedBox from "./WatchedBox/WatchedBox";
+
+export default function Main({ tempMovieData, tempWatchedData }) {
+  return (
+    <main className="main">
+      <ListBox tempMovieData={tempMovieData} />
+      <WatchedBox tempWatchedData={tempWatchedData} />
+    </main>
+  );
+}
