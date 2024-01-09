5fb2c3d9d5d75da370eccd3065669c39975d66c9
nitin devatraj <nitindevatraj@protonmail.com>
2024-01-10 00:09:26 +0530

    use a Ref to focus search input on enter keypress

--- src/components/NavBar/Search.js
+++ src/components/NavBar/Search.js
@@ -1,6 +1,28 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputElRef = useRef(null);
+
+  useEffect(
+    function () {
+      function enterHandler(e) {
+        console.log("here");
+        if (document.activeElement === inputElRef.current) return;
+        if (e.code === "Enter") {
+          inputElRef.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", enterHandler);
+      return () => document.removeEventListener("keydown", enterHandler);
+    },
+    [setQuery]
+  );
+
   return (
     <input
+      ref={inputElRef}
       className="search"
       type="text"
       placeholder="Search movies..."
